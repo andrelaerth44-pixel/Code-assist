66417062409072a71e9a4a58ff9ffb38f51bb3f8
andrelaerth44-pixel <andrelaerth44@gmail.com>
2026-09-18 15:01:31 +0100

    Revert unauthorized NOVA changes

--- app/src/main/java/org/nova/notes/NovaRuntime.kt
+++
@@ -1,2 +0,0 @@
-package org.nova.notes
-class NovaRuntime { init { System.loadLibrary("nova_android") }; external fun nativeVersion(): String; external fun nativeSetText(value:String); external fun nativeGetText():String }
